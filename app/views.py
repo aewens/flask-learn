cccb130ae54ae31916b8e312f2c30764be36548e
aewens <email@aewens.com>
2017-07-31 17:37:51 -0500

    Added templates

--- app/views.py
+++ app/views.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello, world!"
+    user = {"nick": "aewens"}
+    posts = [
+        {
+            "author": {"nick": "aewens"},
+            "body": "Test post for testing the posts."
+        },
+        {
+            "author": {"nick": "admin"},
+            "body": "Posts can now be made, hurray!"
+        }
+    ]
+    return render_template("index.html", title="Home", user=user, posts=posts)
